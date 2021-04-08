9b7234964e268ad6ad7c9fa588ea9adbeb77068f
ChowDPa02k <chowdpa02k@outlook.com>
2021-04-08 11:51:56 +0800

    2.0: add argument parser and readme.md

--- main.py
+++ main.py
@@ -5,24 +5,29 @@ from apscheduler.schedulers import background
 import requests
 from time import sleep
 import signal,os,logging
-from pprint import pprint
 import binascii
 import serial
+import argparse
+
+coins = ['ETHUSDT', 'BTCUSDT']
+price_change= {}
 
 COLOR_UP_GREEN = '7596'
 COLOR_DOWN_RED = '64106'
-# init parameters in remote machine
-variables = {
+# init object parameters in HMI
+# use array is not a good way, but who cares?
+controls = {
     # coin name, change mark, price change percent, 24h highest, 24h lowest, price integer, price dot
     'ETH': ['cn0', 'm0', 'pc0', 'h0', 'l0', 'p0', 'p0d'],
     'BTC': ['cn1', 'm1', 'pc1', 'h1', 'l1', 'p1', 'p1d']
 }
 
 def serial_command_generator(content):
-    global COLOR_DOWN_RED, COLOR_UP_GREEN, variables
+    global COLOR_DOWN_RED, COLOR_UP_GREEN, controls
     # print('got it')
-    parameter = variables[content['currency']]
-    commands = []
+    parameter = controls[content['currency']]
+    # freeze screen while updating partial data might make it looks more... stable?
+    commands = ['ref_stop', ]
 
     # change color if price goes up
     if content['rise'] == 1:
@@ -51,12 +56,17 @@ def serial_command_generator(content):
     
     commands.append(parameter[3] + '.txt="' + str(content['high']) + '"')
     commands.append(parameter[4] + '.txt="' + str(content['low']) + '"')
-
+    # seperate price dot to make ui looks comfortable
     price = str(content['price']).split('.')
     commands.append(parameter[5] + '.txt="' + str(price[0]) + '"')
+    # some part of this 2 price controls overlayed each other, 
+    # we want price after dot rendered at the top, 
+    # so unfreeze before p*d got rendered, then render it,
+    # this make sure that p*d will always be the last control that got updated
+    commands.append('ref_star')
     commands.append(parameter[6] + '.txt=".' + str(price[1]) +' "')
-
-    print(commands)
+    if serial_debug:
+        print(commands)
     return commands
 
 def send_serial(device, commands):
@@ -66,15 +76,15 @@ def send_serial(device, commands):
         device.write(cmd)
 
 def get_price_change():
-    global eth_price_change, btc_price_change
-    coins = ['ETHUSDT', 'BTCUSDT']
+    global coins, price_change
     for item in coins:
         r = requests.get('https://api.binance.com/api/v3/ticker/24hr?symbol=' + item)
         result = r.json()
-        if item == 'ETHUSDT':
-            eth_price_change = result['priceChangePercent']
-        if item == 'BTCUSDT':
-            btc_price_change = result['priceChangePercent']
+        price_change[item] = result['priceChangePercent']
+        # if item == 'ETHUSDT':
+        #     eth_price_change = result['priceChangePercent']
+        # if item == 'BTCUSDT':
+        #     btc_price_change = result['priceChangePercent']
         sleep(0.5)
 
 def on_message(wsapp, message):
@@ -87,27 +97,25 @@ def on_message(wsapp, message):
         wsapp.send(
             json.dumps({"pong": result['ping']})
         )
-    elif 'ch' in result:
+    if 'ch' in result:
         if result['ch'] == 'market.ethusdt.detail':
             currency = 'ETH'
-            change = eth_price_change
+            change = price_change['ETHUSDT']
         if result['ch'] == 'market.btcusdt.detail':
             currency = 'BTC'
-            change = btc_price_change
-        
-        # print(currency, result['tick']['close'], 0 if float(change)<0 else 1,float(change), result['tick']['high'], result['tick']['low'])
-        command = serial_command_generator({
-            'currency': currency,
-            'price': result['tick']['close'],
-            'rise': 0 if float(change)<0 else 1,
-            'change': change,
-            'high': result['tick']['high'],
-            'low': result['tick']['low']
-        })
-        send_serial(device, command)
-        # pprint(command)
-
-    # pprint(result)
+            change = price_change['BTCUSDT']
+        if print_only:
+            print(currency, result['tick']['close'], float(change), result['tick']['high'], result['tick']['low'])
+        else:
+            command = serial_command_generator({
+                'currency': currency,
+                'price': result['tick']['close'],
+                'rise': 0 if float(change)<0 else 1,
+                'change': change,
+                'high': result['tick']['high'],
+                'low': result['tick']['low']
+            })
+            send_serial(device, command)
 
 def on_open(wsapp):
     req_eth = '{"sub": "market.ethusdt.detail", "id": "1110"}'
@@ -124,20 +132,38 @@ def sigint_handler():
     wsapp.close()
 
 if __name__ == "__main__":
+    # argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--serial", dest="serial", help="serial port location, default is /dev/ttyAMA0")
+    parser.add_argument("-l", "--list-only", dest="po", help="only print information rather than sending to serial port", action="store_true")
+    parser.add_argument("--serial-debug", dest="sd", help="show commands that sent to serial port", action="store_true")
+    args = parser.parse_args()
+    
+    print_only = True if args.po else False
+    serial_debug = True if args.sd else False
+    serial_port = args.serial if not (args.serial == None) else '/dev/ttyAMA0'
+    if (print_only and serial_debug):
+        raise ValueError('list_only conflicts with serial port arguments')
+
     # open serial port
-    device = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
+    if not print_only:
+        device = serial.Serial(serial_port, 115200, timeout=1)
+        print('Successfully open serial port', device.name)
 
     # init increses variable
-    eth_price_change = ""
-    btc_price_change = ""
     get_price_change()
+    print('Initialized cryptocurrency price changes:')
+    for item in price_change:
+        print(item, price_change[item])
 
     # create a schedular to get increases
+    print('Creating price change update schedular')
     schedular = background.BackgroundScheduler()
     schedular.add_job(get_price_change, 'interval', seconds=1, id='refresh')
     schedular.start()
 
     # establish websocket connection
+    print('Establishing websocket connection\n')
     websocket.enableTrace(False)
     wsapp = websocket.WebSocketApp("wss://api.huobi.pro/ws", on_message=on_message, on_open=on_open)
     signal.signal(signal.SIGINT, sigint_handler)
---
+++ readme.md
@@ -0,0 +1,62 @@
+# A project to show realtime cryptocurrency price info in HMI USART screen
+
+Now, you don't have to take your phone, open a cryptocurrency market app and wait for seconds until you can check the price. Just simply look at your table:
+
+![preview](images/preview.jpg)
+
+This porject fetch data from both Huobi (using WebSocket) and Binance (using HTTP request), and update to HMI real-time.
+
+Note: both of those market blocked request from Mainland China IP
+
+---
+
+## Hardware
+
+- Raspberry Pi 3 series (or any Linux Devboard)
+- HMI UART Enhanced Screen (TJC-4832K035-011R)
+
+## Installation
+
+For Respberry Pi 3 Series, you need to enable GPIO serial Port to make it works. Otherwise, a CH340 USBTTL also works at any platform, just simply modify serial port name using -s argument.
+
+```
+pip3 install -r requirements.txt
+python3 main.py
+```
+
+Reference: https://blog.csdn.net/m0_37509650/article/details/85403217
+
+## Usage
+
+```
+usage: main.py [-h] [-s SERIAL] [-l] [--serial-debug]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SERIAL, --serial SERIAL
+                        serial port location, default is /dev/ttyAMA0
+  -l, --list-only       only print information rather than sending to serial port
+  --serial-debug        show commands that sent to serial port
+```
+
+## Example
+
+```
+python .\main.py -l
+```
+![example](images/example.png)
+
+## UI
+
+You can change screen UI by modifying ```screen.HMI``` using USART HMI
+
+![screenshot](images/screenshot.png)
+
+Reference: http://wiki.tjc1688.com/doku.php?id=start
+
+---
+
+## Thanks
+
+- Myself
+- Weipu Liu
